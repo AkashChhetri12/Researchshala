fc1b1fdf6d28691aa07a328dfec2f66dd05a5059
AkashChhetri12 <chhetriakash2616@gmail.com>
2019-05-14 13:06:50 +0530

    Added comments on box.js

--- box.js
+++ box.js
@@ -6,16 +6,31 @@ var container = ["container1", "container2"];
 var i = 0;
 var j=0;
 var offset = 0;
+
+// Function that will work on keystrokes
 function myfunction(){
     var box = document.getElementById("box")
     var msg = document.getElementById("msg")
     var cont = document.getElementById("cont")
+
     // console.log("replace")
+
+
+    // Toggleing containers/Dialog boxes
+    // As show in refernce video one dialog box at a time
     toggleClass(cont,container[0],container[1]);
+
+    // for switching the image source of container
     box.src = imgsrc[++i % 2];
+
+    // Putting messages onto the dialog box
     msg.innerHTML = message[++j % 4];
+
+
     // console.log("done")
 
+
+    // Shifting background relative to the dialog boxes
     var body = document.getElementById("b")
     if(offset == 0){
         body.style.backgroundPositionX = 40+"%";
@@ -31,6 +46,8 @@ function myfunction(){
 
 }
 
+
+// this function is for toggling the css classes
 function toggleClass(ele, cls1, cls2) {
    if(ele.className == cls1)
     ele.className = cls2;
